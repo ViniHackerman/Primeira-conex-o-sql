de38cb0cd56f7813a8a9d290ccfdaaf291f962bd
vinicius <viniciusgouveamachado@gmail.com>
2023-10-24 22:14:54 -0300

    Primeiro salvamento

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express()
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded)({
+    extended: true
+})
+
+app.use(express.json())
+
+app.get("/", (requisicao, resposta) =>{
+    resposta.render("home")
+})
+
+
+const conn = mysql.createConnection({
+
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql",
+    port: 3307
+
+})
+
+conn.conecct((error)=>{
+    if(error) {
+        console.log(error)
+        return
+    }
+
+    console.log("Conectado ao Mysql!")
+
+    app.listen(3000,() => {
+        console.log("Servidor rodando na porta 3000!")
+    })
+})
